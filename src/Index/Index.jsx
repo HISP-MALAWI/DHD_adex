3677d8de4e708bccab4be45f963f73f73e075502
Yambanso <yambanso22@gmail.com>
2024-02-05 15:11:36 +0200

    import home.jsx

--- src/Index/Index.jsx
+++ src/Index/Index.jsx
@@ -1,22 +1,46 @@
-import { ButtonStrip, Center } from '@dhis2/ui';
+import { ButtonStrip, Center, CircularLoader, Layer } from '@dhis2/ui';
 import React,{useEffect,useState} from 'react';
 import Header from '../widgets/header.widget';
 import InitiateTransaction from './pages/initiateTransaction';
+import { useDataEngine } from '@dhis2/app-runtime';
+import Home from './pages/Home/home';
 
 function Index(props) {
+    const engine = useDataEngine()
+    const [loading, setLoading] = useState(true)
     const [page,setPage] = useState('index')
+    const [initialisations, setInitalisations] = useState()
+
+    const fetchTransactions = async() => {
+        const dataStoreKeys = props?.data?.dataStore
+        if(dataStoreKeys.includes('GF_transactions')){
+            const query = { dataStore: {
+                resource: "dataStore/GF_transactions",
+                params: {
+                  paging: false,
+                  fields: ["*"],
+                },
+              },
+            }
+            const res = await engine.query(query);
+
+        }
+        setLoading(false)
+    }
     useEffect(()=>{
-        console.log(props)
+        fetchTransactions()
     },[])
     return (
         <div>
+
         <Header page={page} setPage={setPage} styles={props?.styles}/>
-        {page === 'index' ?
-            <>
-            <Center>
-                Home
-            </Center>
-            </>
+       {loading ? <Layer translucent>
+        <Center>
+            <CircularLoader />
+        </Center>
+       </Layer>: 
+        <>{page === 'index' ?
+            <Home initialisations={initialisations} />
             :
             <>
             {page === 'init' ? 
@@ -27,7 +51,7 @@ function Index(props) {
             </>}
             </>
 
-        }    
+        }</>}    
         </div>
     );
 }
