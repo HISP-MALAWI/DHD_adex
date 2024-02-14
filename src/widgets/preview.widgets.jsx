f12f0f5821a012689a2a54a4310fe701eaea995e
Yambanso <yambanso22@gmail.com>
2024-02-14 14:31:28 +0200

    rendering analytics data

--- src/widgets/preview.widgets.jsx
+++ src/widgets/preview.widgets.jsx
@@ -11,39 +11,102 @@ import {
 } from "@dhis2/ui";
 import GetAnalytics from "../Services/data/store/analytics";
 import { useDataEngine } from "@dhis2/app-runtime";
+import _, { values } from 'lodash'
 import DataElementGroups from "../Services/data/store/dataElementGroups";
 
 export default function Preview(props) {
+  const [analytics,setAnalytics] = useState([])
+  const [vs,setVs] = useState([])
+  let peArr = []
+
+  const prepareAnalytics = (analytics) => {
+    const rows = analytics?.rows  
+    const periods = Object.values(analytics?.metaData?.items).filter(pe => pe?.dimensionItemType === "PERIOD")
+    const dataElements = Object.values(analytics?.metaData?.items).filter(pe => pe?.dimensionItemType === "DATA_ELEMENT")
+    const periodsDataValues = _.groupBy(rows,function(row){
+      return row[2]
+    })
+    var values = []
+    const groups = Object.values(periodsDataValues)
+    groups.map(group => {
+      let pe = null
+      var objects =[]
+      group.map(grp => {
+        pe = periods.filter(pe => pe.uid === grp[2])[0]
+        const dataElement = dataElements.filter(de => de.uid === grp[0])[0]
+        objects.push({indicatorName : dataElement?.name,indicatorCode : dataElement?.code,dataValues: grp[3],period : pe?.name})
+      })
+      values.push({period : pe?.name,dataValues:objects})
+    })
+    setAnalytics(values)
+  }
 
   useEffect(()=>{
-    console.log(props)
+    prepareAnalytics(props?.analytics)    
   },[])
+
+  useEffect(()=>{
+    let x = []
+    analytics.map(an => x.push(...an.dataValues))
+    const grps = _.groupBy(x,function(vs){
+      return vs?.indicatorCode.split('-')[0]
+    })
+    setVs(Object.values(grps))
+  },[analytics])
+  
+  
   return (
-    <div>
+    <div style={{
+      width : "100%",
+      overflow : "scrollS"
+    }}>
       <Table>
         <TableHead>
+          
           <TableRowHead>
-            <TableCellHead>Indicator Name</TableCellHead>
-            <TableCellHead>Closing Balance (Stock on Hand)</TableCellHead>
-            <TableCellHead>Losses</TableCellHead>
-            <TableCellHead>(+ve) Adjustment</TableCellHead>
-            <TableCellHead>(-ve) Adjustment</TableCellHead>
-            <TableCellHead>Quantity Used in Period</TableCellHead>
-            <TableCellHead>Stocked Out Days</TableCellHead>
-            <TableCellHead>Quantity Received</TableCellHead>
+            {analytics.map((val,index) =>{2022
+              console.log(index)
+              return( 
+            <TableCellHead colSpan={index== 0 ? '5': '4'} key={val?.period}>
+              <div style={{
+                textAlign : 'center'
+              }}>{val?.period}</div>
+            </TableCellHead>
+                )})}
           </TableRowHead>
+          <TableRowHead>
+          <TableCellHead >Dataelement Name</TableCellHead>
+            {analytics.map((index)=>{      
+            return(
+             <>
+            <TableCellHead key={'Closing Balance'}>Closing Balance (Stock on Hand)</TableCellHead>
+            <TableCellHead key={'qty'}>Quantity Used in Period</TableCellHead>
+            <TableCellHead key={'stk'}>Stock Out Days</TableCellHead>
+            <TableCellHead key={'rc'}>Quantity Received</TableCellHead>
+            </>
+            )} )}
+          </TableRowHead>          
         </TableHead>
         <TableBody>
+        {vs.map(val =>{
+          const pe =_.groupBy(val,function(val){
+            return val.period
+          })
+          const v = Object.entries(pe)
+          const arr = val[0]?.indicatorName.split(' ').slice(1,-3)
+          return(
           <TableRow>
-            <TableCell>Acyclovir 200mg, tablets</TableCell>
-            <TableCell>{Math.floor(Math.random() * (10000 - 100))}</TableCell>
-            <TableCell>{Math.floor(Math.random() * (10000 - 100))}</TableCell>
-            <TableCell>{Math.floor(Math.random() * (10000 - 100))}</TableCell>
-            <TableCell>{Math.floor(Math.random() * (10000 - 100))}</TableCell>
-            <TableCell>{Math.floor(Math.random() * (10000 - 100))}</TableCell>
-            <TableCell>{Math.floor(Math.random() * (10000 - 100))}</TableCell>
-            <TableCell>{Math.floor(Math.random() * (10000 - 100))}</TableCell>
+            <TableCell>{arr.join(' ')}</TableCell>
+            {v.map(va =>{
+            return(<>            
+            <TableCell>{va[1].filter(el => el?.indicatorName.toLocaleLowerCase().includes('Stock on Hand'.toLocaleLowerCase()))[0].dataValues}</TableCell>
+            <TableCell>{va[1].filter(el => el?.indicatorName.toLocaleLowerCase().includes('Quantity used'.toLocaleLowerCase()))[0]?.dataValues}</TableCell>
+            <TableCell>{va[1].filter(el => el?.indicatorName.toLocaleLowerCase().includes('Stock Out Days'.toLocaleLowerCase()))[0]?.dataValues}</TableCell>
+            <TableCell>{va[1].filter(el => el?.indicatorName.toLocaleLowerCase().includes('Quantity Received'.toLocaleLowerCase()))[0]?.dataValues}</TableCell>
+            </>
+            )})}
           </TableRow>
+          )})}
         </TableBody>
         <TableFoot>
           <TableRow></TableRow>
