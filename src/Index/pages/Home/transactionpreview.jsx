6bfd36f9cc4d1adc6cc4a0ac899f7a3d61f57240
Yambanso <yambanso22@gmail.com>
2024-02-24 20:55:51 +0200

    stacked table instead of Notice Box

--- src/Index/pages/Home/transactionpreview.jsx
+++ src/Index/pages/Home/transactionpreview.jsx
@@ -1,5 +1,5 @@
-import { Box, Card, NoticeBox, Button } from "@dhis2/ui";
-import { Link} from "react-router-dom";
+import { Box, Card, NoticeBox, Button, StackedTable, StackedTableHead, StackedTableRowHead, StackedTableCellHead, StackedTableBody, StackedTableRow, StackedTableCell } from "@dhis2/ui";
+import { Link, useLocation} from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import Preview from "../../../widgets/preview.widgets";
 import { useDataEngine } from "@dhis2/app-runtime";
@@ -7,6 +7,7 @@ import { useParams } from "react-router-dom/dist";
 import { StringParam, useQueryParams } from "use-query-params";
 
 export default function TransactionPreview(props) {
+  const location = useLocation()
   const [loading, setLoading] = useState(true);
   const engine = useDataEngine();
   const [transactions, setTransactions] = useState([]);
@@ -18,10 +19,10 @@ export default function TransactionPreview(props) {
   });
   const { id } = transactionIdQuery;
   //  saved transactions from datastore
-  const getTransactions = async () => {
+  const getTransactions = async (key) => {
     const myQuery = {
       dataStore: {
-        resource: "dataStore/OpenLMIS_SnowFlake_Intergration",
+        resource: `dataStore/OpenLMIS_SnowFlake_Intergration/${key}`,
         params: {
           paging: false,
           fields: ["."],
@@ -30,6 +31,7 @@ export default function TransactionPreview(props) {
     };
     try {
       const res = await engine.query(myQuery);
+      console.log(res)
       if (res.dataStore == undefined || res.dataStore == null) {
         setLoading(true);
       } else {
@@ -45,16 +47,16 @@ export default function TransactionPreview(props) {
     }
   };
   useEffect(() => {
-    getTransactions();
-    console.log(
-      transactions.filter((transaction) => transaction?.value?.id == id)[0]
-    );
-  }, [transactions]);
+    getTransactions(location.search.split('=')[1]);    
+  }, []);
 
+  useEffect(()=>{
+    console.log(transactions)
+  },[transactions])
   return (
     <div>
       <div style={{width: "100", backgroundColor: "#f1f2f5", padding: 10}}>
-        <Button small primary>
+        <Button basic>
           <Link to={"/"} style={{textDecoration: "none", color: "#fff"}}>Back</Link>
         </Button>
       </div>
@@ -62,28 +64,42 @@ export default function TransactionPreview(props) {
         className=""
         style={{ display: "flex", flexDirection: "row", alignItems: "center", padding: 10, flexWrap: "wrap", justifyContent: "center", gap: 10 }}
       >
-        <div>
-          <NoticeBox title="Transaction Identification">
-           {id}
-          </NoticeBox>
-        </div>
-        <div className="">
-          <NoticeBox title="Description">
-            {transactions?.filter((transaction) => transaction?.value?.id == id)[0]?.value.description}
-          </NoticeBox>
-        </div>
-        <div className="">
-          <NoticeBox title="Creadted By:">
-            {transactions?.filter((transaction) => transaction?.value?.id == id)[0]?.value.user_id.name}
-          </NoticeBox>
-        </div>
-        <div className="">
-          <NoticeBox title="Status Summary:">
-             {transactions?.filter((transaction) => transaction?.value?.id == id)[0]?.value.status}
-          </NoticeBox>
-        </div>
+        <StackedTable>
+          <StackedTableHead>
+            <StackedTableRowHead>
+              <StackedTableCellHead>
+                Id
+              </StackedTableCellHead>
+              <StackedTableCellHead>
+                Name
+              </StackedTableCellHead>
+              <StackedTableCellHead>
+                Description
+              </StackedTableCellHead>
+              <StackedTableCellHead>
+                Status
+              </StackedTableCellHead>
+            </StackedTableRowHead>
+          </StackedTableHead>
+          <StackedTableBody>
+            <StackedTableRow>
+              <StackedTableCell>
+                {transactions.id}
+              </StackedTableCell>
+              <StackedTableCell>
+                {transactions.name}
+              </StackedTableCell>
+              <StackedTableCell>
+                {transactions.description}
+              </StackedTableCell>
+              <StackedTableCell>
+                {transactions.status}
+              </StackedTableCell>
+            </StackedTableRow>
+          </StackedTableBody>
+        </StackedTable>
       </div>
-      <div className="">
+      {/* <div className="">
         {transactions.filter(
           (transaction) => transaction?.value?.id == id
         )[0] == null ||
@@ -92,16 +108,16 @@ export default function TransactionPreview(props) {
           )[0] == undefined ? (
             <spa></spa>
           ) : (
-            <Preview
-              analytics={
-                transactions.filter(
-                  (transaction) => transaction?.value?.id == id
-                )[0]?.value?.analytics
-              }
-              styles={props.styles}
-            />
+            // <Preview
+            //   analytics={
+            //     transactions.filter(
+            //       (transaction) => transaction?.value?.id == id
+            //     )[0]?.value?.analytics
+            //   }
+            //   styles={props.styles}
+            // />
           ))}
-      </div>
+      </div> */}
     </div>
   );
 }
