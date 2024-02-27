46c2bbd606c52d45ea850d750bd16530b50ed31c
Yambanso <yambanso22@gmail.com>
2024-02-27 15:05:52 +0200

    changes

--- d2.config.js
+++ d2.config.js
@@ -1,9 +1,11 @@
 const config = {
     type: 'app',
-
     entryPoints: {
         app: './src/App.js',
     },
+    icon:"../dhis2-app-icon.png",
+    name:"OpenLMIS-Snowflakes-Intergration",
+    author: "Hisp Unima"
 }
 
 module.exports = config
