8f8914d3eb0d7cfce8c3b7e6dc71ff6fc2385f79
Yambanso <yambanso22@gmail.com>
2024-04-30 11:18:52 +0200

    fetching using group id

--- src/Services/data/store/analytics.js
+++ src/Services/data/store/analytics.js
@@ -1,22 +1,18 @@
 const url =
   "https://dhis2-staging.kuunika.org:8087/api/40/analytics?dimension=dx%3AFX640XO5V1C%3BKYVN98396QE%3B317H768X27B%3B0YY71FP4061%3B6OB7IWP255Y%3B5252T5C35SI%3BA4YEI2Q2PE8%3BT8XJ6AQ1WB4%3B79J4V0T3JZ2%3BR1J184BQHN2%3BCD2RNQCX1B1%3BU15D2KT93SU,ou%3AlZsCb6y0KDX,pe%3A202203&displayProperty=NAME&includeNumDen=true&skipMeta=false&skipData=false&includeMetadataDetails=true";
 let GetAnalytics = {
-  analytics: (engine, dataElements, period, orgUnit, orgUnits) => {
+  analytics: (engine, dataElements, period, groupID) => {
     let joinedDataElements = [];
     let joinedOrgUnits = [];
 
     dataElements?.map((dataElement) => {
       joinedDataElements.push(dataElement);
     });
-    orgUnits?.map((orgUnit) => {
-      joinedOrgUnits.push(orgUnit?.id);
-    });
     let dataDimension = joinedDataElements?.join(";");
     let joinedPE = period.join(";");
-    let orgUnitsJoined = joinedOrgUnits.join(";");
     const analystisQuery = {
       analytics: {
-        resource: `analytics?dimension=dx:${dataDimension},ou:${orgUnit},pe:${joinedPE}&displayProperty=NAME&includeNumDen=true&skipMeta=false&skipData=false&includeMetadataDetails=true`,
+        resource: `analytics?dimension=dx:${dataDimension},ou:OU_GROUP-${groupID},pe:${joinedPE}&displayProperty=NAME&includeNumDen=true&skipMeta=false&skipData=false&includeMetadataDetails=true`,
         params: {},
       },
     };
